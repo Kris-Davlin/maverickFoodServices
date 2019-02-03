a8d06a4e76e2902f5cfbd2c3359c34125a982de4
Kristopher Davlin <kdavlin@unomaha.edu>
2019-02-03 17:54:50 -0600

    Added static_root

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
